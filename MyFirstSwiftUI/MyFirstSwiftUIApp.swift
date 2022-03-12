914f3742b1bf306f1bea7cbfe39426d7ffb4d317
Ferruh <ferruh.cihan@westerops.com>
2022-03-13 02:49:44 +0300

    Initial Commit

---
+++ MyFirstSwiftUI/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  MyFirstSwiftUI
+//
+//  Created by yoda on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
